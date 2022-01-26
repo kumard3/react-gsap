f279132e92de38300be08bd293fe5d7a59fcedde
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-26 23:54:46 +0530

    layer pin function added

--- react-gsap-scrolltrigger/src/components/LayeredPinning.js
+++ react-gsap-scrolltrigger/src/components/LayeredPinning.js
@@ -10,6 +10,20 @@ export default function LayeredPinning() {
     let panels = gsap.utils.toArray(".panel"),
       scrollTween;
 
+    let sections = gsap.utils.toArray(".panel");
+
+    gsap.to(sections, {
+      xPercent: -100 * (sections.length - 1),
+      ease: "none",
+      scrollTrigger: {
+        trigger: sections,
+        pin: true,
+        scrub: 1,
+        snap: 1 / (sections.length - 1),
+        // base vertical scrolling on how wide the container is so it feels more natural.
+        end: "+=3500",
+      },
+    });
     function goToSection(i) {
       scrollTween = gsap.to(window, {
         scrollTo: { y: i * innerHeight, autoKill: false },
@@ -36,8 +50,36 @@ export default function LayeredPinning() {
     });
   }, []);
   return (
-    <div>
-      <div className="flex flex-col text-center h-[10rem] bg-blue-400  text-xl py-10">
+    <>
+
+
+<>
+<div class="container">
+        <div class="description panel blue">
+          <div>
+            <h1>Horizontal snapping sections (simple)</h1>
+            <p>
+              Scroll vertically to scrub the horizontal animation. It also
+              dynamically snaps to the sections in an organic way based on the
+              velocity. The snapping occurs based on the natural ending position
+              after momentum is applied, not a simplistic "wherever it is when
+              the user stops".
+            </p>
+            <div class="scroll-down">
+              Scroll down<div class="arrow"></div>
+            </div>
+          </div>
+        </div>
+
+
+        <section class="panel bg-red-600">ONE</section>
+      <section class="panel bg-orange-600">TWO</section>
+      <section class="panel bg-purple-600">THREE</section>
+      <section class="panel bg-green-600">FOUR</section>
+      </div>
+</>
+
+      {/* <div className="flex flex-col text-center h-[10rem] bg-blue-400  text-xl py-10">
         <div>
           <h1>Layered pinning</h1>
           <p>Use pinning to layer panels on top of each other as you scroll.</p>
@@ -47,6 +89,7 @@ export default function LayeredPinning() {
           </div>
         </div>
       </div>
+ 
 
       <section class="panel bg-red-600">ONE</section>
       <section class="panel bg-orange-600">TWO</section>
@@ -62,7 +105,7 @@ export default function LayeredPinning() {
             height={64}
           />
         </a>
-      </header>
-    </div>
+      </header> */}
+    </>
   );
 }
